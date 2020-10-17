473d7a1b26ad10bb7200b32e1c8c5026ce8525a6
Juan Pablo <juanpa-1920@hotmail.com>
2020-10-16 21:00:03 -0500

    [ADD] Configuracion de firebase añadida

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCwB8BHH63N1EfIB55w8XZDDZhoAIiQ5OE",
+  authDomain: "todo-list-51868.firebaseapp.com",
+  databaseURL: "https://todo-list-51868.firebaseio.com",
+  projectId: "todo-list-51868",
+  storageBucket: "todo-list-51868.appspot.com",
+  messagingSenderId: "1036826288188",
+  appId: "1:1036826288188:web:d0be74a1669a18b41e6339",
+  measurementId: "G-TD5WNVG8WP"
+};
