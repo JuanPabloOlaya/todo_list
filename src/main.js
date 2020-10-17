94f6eae92ad2a3b46669ed075564779476eb1e6b
Juan Pablo <juanpa-1920@hotmail.com>
2020-10-17 08:47:20 -0500

    [REMOVE] Dependencia vuefire removida

--- src/main.js
+++ src/main.js
@@ -3,11 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import VueFire from "vuefire";
 
 Vue.config.productionTip = false;
-
-VisualViewport.use(VueFire);
 
 new Vue({
   router,
